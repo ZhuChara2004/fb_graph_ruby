8f300c88742bef2021e59dfeeab1d1a439ed8f2d
Dmitry Zhuk <zhuchara@gmail.com>
2017-06-12 11:14:55 +0300

    Initial commit

---
+++ lib/fb_graph_ruby.rb
@@ -0,0 +1,5 @@
+require "fb_graph_ruby/version"
+
+module FbGraphRuby
+  # Your code goes here...
+end
---
+++ spec/fb_graph_ruby_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe FbGraphRuby do
+  it "has a version number" do
+    expect(FbGraphRuby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
