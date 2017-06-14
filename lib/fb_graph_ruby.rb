c0829d2aa6976312e94227cefaa0a4d57b1b4d51
Olha Bezpalchuk <olha.bezpalchuk@gmail.com>
2017-06-14 11:38:25 +0300

    Getting events added

--- lib/fb_graph_ruby.rb
+++ lib/fb_graph_ruby.rb
@@ -90,6 +90,12 @@ module FbGraphRuby
       query = { fields: 'id', access_token: access_token }
       HTTParty.get(uri, query: query).parsed_response
     end
+
+    def self.get_events(fb_id, access_token)
+      uri = "https://graph.facebook.com/v2.9/#{fb_id}/events"
+      query = { fields: 'body,paging', access_token: access_token }
+      HTTParty.get(uri, query: query).parsed_response
+    end
   end
 
 end
